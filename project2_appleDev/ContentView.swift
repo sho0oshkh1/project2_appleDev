15d4022fd99fc49f481a23dd8b3eeef78317da58
shoug <sho0oshkh@gmail.com>
2023-10-08 09:29:26 +0300

    Initial Commit

---
+++ project2_appleDev/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  project2_appleDev
+//
+//  Created by shoug on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project2_appleDev/project2_appleDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  project2_appleDevApp.swift
+//  project2_appleDev
+//
+//  Created by shoug on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct project2_appleDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
